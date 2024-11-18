16b68cf6444e7b646f81a1be583bfa95adecd5ed
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-11-18 16:55:31 +0530

    Frontend setup completed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-5xl text-red-500">Hello</h1>
+      <Button variant={"outline"}>Send</Button>
+    </>
+  )
+}
+
+export default App
