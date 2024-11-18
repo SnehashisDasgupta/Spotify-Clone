fc2ef05af5bb2c9223458b282e9363787c3e1ca5
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-11-18 20:20:24 +0530

    Auth setup using 'Clerk'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,29 @@
-import { Button } from "./components/ui/button"
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { Button } from "./components/ui/button";
 
 function App() {
-  
-
   return (
     <>
-      <h1 className="text-5xl text-red-500">Hello</h1>
-      <Button variant={"outline"}>Send</Button>
+      <header>
+        {/* If user is Signed Out, show SignIn  */}
+        <SignedOut>
+          <SignInButton>
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+
+        {/* If user is Signed In, show SignOut  */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
