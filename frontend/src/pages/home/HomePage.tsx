d886fe51d8e34bb9b8ae0af46d6b7aab553377ee
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-11-28 16:29:18 +0530

    Home page (frontend) completed

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,10 +1,43 @@
 import Topbar from "@/components/Topbar"
+import { useMusicStore } from "@/stores/useMusicStore"
+import { useEffect } from "react";
+import FeaturedSection from "./components/FeaturedSection";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import SectionGrid from "./components/SectionGrid";
 
 const HomePage = () => {
+  const {
+    isLoading,
+    featuredSongs,
+    madeForYouSongs,
+    trendingSongs,
+    fetchFeaturedSongs,
+    fetchMadeForYouSongs,
+    fetchTrendingSongs
+  } = useMusicStore();
+
+  useEffect(() => {
+    fetchFeaturedSongs();
+    fetchMadeForYouSongs();
+    fetchTrendingSongs();
+  }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
+
+
   return (
-    <div>
+    <main className="rounded-lg overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900">
       <Topbar />
-    </div>
+      <ScrollArea className="h-[calc(100vh-180px)]">
+        <div className="p-4 sm:p-6">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">Good Morning</h1>
+          <FeaturedSection />
+
+          <div className="space-y-8">
+            <SectionGrid title="Made For You" songs={madeForYouSongs} isLoading={isLoading} />
+            <SectionGrid title="Trending" songs={trendingSongs} isLoading={isLoading} />
+          </div>
+        </div>
+      </ScrollArea>
+    </main>
   )
 }
 
--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,4 +1,5 @@
 import { axiosInstance } from "@/lib/axios";
+import { useAuthStore } from "@/stores/useAuthStore";
 import { useAuth } from "@clerk/clerk-react";
 import { Loader } from "lucide-react";
 import { useEffect, useState } from "react";
@@ -6,43 +7,43 @@ import { useEffect, useState } from "react";
 const updateApiToken = (token: string | null) => {
   if (token) {
     // Sets the 'Authorization' header of the Axios instance to 'Bearer <token>' for all future requests
-    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   } else {
     // Removes the 'Authorization' header to prevent sending invalid or unnecessary tokens
-    delete axiosInstance.defaults.headers.common['Authorization']
+    delete axiosInstance.defaults.headers.common["Authorization"];
   }
-}
+};
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const { getToken } = useAuth();
   const [loading, setLoading] = useState(true);
+  const { checkAdminStatus } = useAuthStore();
 
   useEffect(() => {
     const initAuth = async () => {
       try {
         const token = await getToken();
         updateApiToken(token);
-
+        if (token) await checkAdminStatus();
       } catch (error) {
         updateApiToken(null);
         console.log("Error in AuthProvider", error);
       } finally {
         setLoading(false);
       }
-    }
+    };
 
     initAuth();
   }, [getToken]);
 
-  if (loading) return (
-    <div className="h-screen w-full flex items-center justify-center">
-      <Loader className="size-8 text-emerald-500 animate-spin" />
-    </div>
-  )
+  if (loading)
+    return (
+      <div className="h-screen w-full flex items-center justify-center">
+        <Loader className="size-8 text-emerald-500 animate-spin" />
+      </div>
+    );
 
-  return (
-    <>{children}</>
-  )
-}
+  return <>{children}</>;
+};
 
-export default AuthProvider
+export default AuthProvider;
