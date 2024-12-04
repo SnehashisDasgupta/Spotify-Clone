7088edf948100372df05969d36cca9e58dc7cbf3
Snehashis Dasgupta <snehashis.jisu.cse@gmail.com>
2024-12-04 12:30:43 +0530

    Playback controls component added

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,45 +1,72 @@
-import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
-import { Outlet } from "react-router-dom"
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { Outlet } from "react-router-dom";
 import LeftSidebar from "./components/LeftSidebar";
 import FriendsActivity from "./components/FriendsActivity";
 import AudioPlayer from "./components/AudioPlayer";
+import PlaybackControls from "./components/PlaybackControls";
+import { useEffect, useState } from "react";
 
 const MainLayout = () => {
-    const isMobile = false;
+    const [isMobile, setIsMobile] = useState(false);
 
-  return (
-    <div className="flex flex-col h-screen bg-black text-white">
-        
-        <ResizablePanelGroup direction="horizontal" className="flex-1 flex h-full overflow-hidden p-2">
+    useEffect(() => {
+        const checkMobile = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
 
-            <AudioPlayer />
+        checkMobile();
+        window.addEventListener("resize", checkMobile);
+        return () => window.removeEventListener("resize", checkMobile);
+    }, []);
 
-            {/* left sidebar */}
-            <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
-                <LeftSidebar />
-            </ResizablePanel>
+    return (
+        <div className="flex flex-col h-screen bg-black text-white">
+            <ResizablePanelGroup
+                direction="horizontal"
+                className="flex-1 flex h-full overflow-hidden p-2"
+            >
+                <AudioPlayer />
 
+                {/* left sidebar */}
+                <ResizablePanel
+                    defaultSize={20}
+                    minSize={isMobile ? 0 : 10}
+                    maxSize={30}
+                >
+                    <LeftSidebar />
+                </ResizablePanel>
 
-            <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+                <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
 
+                {/* main content */}
+                <ResizablePanel defaultSize={isMobile ? 80 : 60}>
+                    <Outlet />
+                </ResizablePanel>
 
-            {/* main content */}
-            <ResizablePanel defaultSize={isMobile ? 80 : 60}>
-                <Outlet />
-            </ResizablePanel>
+                {!isMobile && (
+                    <>
+                        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
 
+                        {/* right sidebar */}
+                        <ResizablePanel
+                            defaultSize={20}
+                            minSize={0}
+                            maxSize={25}
+                            collapsedSize={0}
+                        >
+                            <FriendsActivity />
+                        </ResizablePanel>
+                    </>
+                )}
+            </ResizablePanelGroup>
 
-            <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
-            
-            
-            {/* right sidebar */}
-            <ResizablePanel defaultSize={20} minSize={0} maxSize={25} collapsedSize={0}>
-                <FriendsActivity />
-            </ResizablePanel>
+            <PlaybackControls />
+        </div>
+    );
+};
 
-        </ResizablePanelGroup>
-    </div>
-  )
-}
-
-export default MainLayout
+export default MainLayout;
--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -4,22 +4,33 @@ import { useEffect } from "react";
 import FeaturedSection from "./components/FeaturedSection";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import SectionGrid from "./components/SectionGrid";
+import { usePlayerStore } from "@/stores/usePlayerStore";
 
 const HomePage = () => {
   const {
     isLoading,
     madeForYouSongs,
     trendingSongs,
+    featuredSongs,
     fetchFeaturedSongs,
     fetchMadeForYouSongs,
     fetchTrendingSongs
   } = useMusicStore();
+
+  const { initializeQueue } = usePlayerStore();
 
   useEffect(() => {
     fetchFeaturedSongs();
     fetchMadeForYouSongs();
     fetchTrendingSongs();
   }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
+
+  useEffect(() => {
+    if (madeForYouSongs.length > 0 && trendingSongs.length > 0 && featuredSongs) {
+      const allSongs = [...madeForYouSongs, ...trendingSongs, ...featuredSongs];
+      initializeQueue(allSongs);
+    }
+  }, [initializeQueue, madeForYouSongs, trendingSongs, featuredSongs]);
 
 
   return (
